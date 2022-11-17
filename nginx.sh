88348655ba50fc5d95954a654d56bfc3f8088be0
Adenitire Ayomikun <adenitiree@gmail.com>
2022-11-17 18:37:33 +0100

    - adjusted nginx pipeline configuration

--- nginx.sh
+++ nginx.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo cp -rf app.conf /etc/nginx/conf.d
+sudo cp -rf app /etc/nginx/sites-available
+sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
+
 chmod 710 /var/lib/jenkins/workspace/django_jenkins
 
 sudo nginx -t
