cfbf6798cd8efadd47644d12f6d82b0d17fe29d4
Adenitire Ayomikun <adenitiree@gmail.com>
2022-11-17 19:06:54 +0100

    - adjusted nginx pipeline configuration

--- nginx.sh
+++ nginx.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo cp -rf web_nginx /etc/nginx/sites-available
-if [ -d "/etc/nginx/sites-enabled/web_nginx" ]; then
+ENABLED_WEB_NGINX=/etc/nginx/sites-enabled/web_nginx
+if test -f "$ENABLED_WEB_NGINX"; then
   echo "NGINX WEB SCRIPT ALREADY EXIST INSIDE ENABLED FILE, NOT CREATING SYMBOLIC"
 else
   sudo ln -s /etc/nginx/sites-available/web_nginx /etc/nginx/sites-enabled
